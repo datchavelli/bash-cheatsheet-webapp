f437831aadccf1fd77823538f54c2cdd9bf232d8
Danilo Zdravkovic <danilo@myoffice.rs>
2025-10-17 11:38:05 +0200

    Add project files.

---
+++ BashCheatSheat/MainForm.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace BashCheatsheet
+{
+    partial class MainForm
+    {
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            if (disposing && (webView != null))
+            {
+                webView.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            // webView
+            this.webView.CreationProperties = null;
+            this.webView.Name = "webView";
+            this.webView.TabIndex = 0;
+            this.webView.Dock = System.Windows.Forms.DockStyle.Fill; //  leave this
+
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(900, 700);
+            this.Controls.Add(this.webView);
+            this.Name = "MainForm";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Bash Cheatsheet";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ BashCheatSheat/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
+using Microsoft.Web.WebView2.Core;
+
+namespace BashCheatsheet
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+
+        private async void InitializeWebView()
+        {
+            webView.Dock = DockStyle.Fill;
+            await webView.EnsureCoreWebView2Async();
+            webView.CoreWebView2.Navigate("https://devhints.io/bash");
+
+            webView.CoreWebView2.NewWindowRequested += (s, e) =>
+            {
+                e.Handled = true;
+                webView.CoreWebView2.Navigate(e.Uri);
+            };
+        }
+    }
+}
